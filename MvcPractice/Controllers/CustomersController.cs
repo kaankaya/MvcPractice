07e275848e0857807392b9de32a795d8aadd141a
Kaan Kaya <kaankaya.0707@gmail.com>
2024-09-02 14:06:42 +0300

    Add project files.

---
+++ MvcPractice/Controllers/CustomersController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcPractice.Models;
+
+namespace MvcPractice.Controllers
+{
+    public class CustomersController : Controller
+    {
+        public IActionResult Index()
+        {
+            //Model Örneği Oluştur
+            var customer = new Customer
+            {
+                Id = 1,
+                FirstName = "Sertan",
+                LastName = "Bozkuş",
+                Email = "sertan.bozkus@example.com"
+            };
+            var orders = new List<Order>
+                {
+                     new Order { Id = 1, ProductName = "Laptop", Price = 999.99m, Quantity = 1 },
+                     new Order { Id = 2, ProductName = "Mouse", Price = 19.99m, Quantity = 2 }
+                };
+            //ViewModel oluşturup view'a gönderiyoruz
+            var viewModel = new CustomerViewModel
+            {
+                Customer = customer,
+                Orders = orders,
+                WelcomeMessage = "Welcome to our customer portal!"
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ MvcPractice/Models/CustomerViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MvcPractice.Models
+{
+    public class CustomerViewModel
+    {
+        public Customer Customer { get; set; }
+        public List<Order> Orders { get; set; }
+        public string WelcomeMessage { get; set; } //Ekstra bir veri
+    }
+}
